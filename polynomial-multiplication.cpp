c85f2f49df4c71413c1efab8a64d63659e51395a
Sameer <52498120+onekokun@users.noreply.github.com>
2020-09-26 13:08:20 +0530

    Update and rename raw_polynomial-multiplication.cpp to polynomial-multiplication.cpp

--- polynomial-multiplication.cpp
+++ polynomial-multiplication.cpp
@@ -39,17 +39,47 @@ void print(struct node* head)
     cout<<0<<endl;
 }
 
+bool conExp(struct node* temp,int exponent)
+{
+
+    bool exist = false;
+    while(temp!=NULL)
+    {
+        if(temp->exp == exponent)
+            exist = true;
+        temp=temp->next;
+    }
+    return exist;
+}
+
 void multiply(struct node* ptr1, struct node* ptr2,struct node** ptr)
 {
+
     while(ptr1!=NULL && ptr2!=NULL)
     {
         struct node* temp = ptr2;
+
         while(ptr1!=NULL)
         {
             ptr2 = temp;
             while(ptr2!=NULL)
             {
-                make(ptr1->coeff*ptr2->coeff,ptr1->exp + ptr2->exp,ptr);
+                int exponent = ptr1->exp+ptr2->exp;
+                int coefficient = ptr1->coeff*ptr2->coeff;
+                if(!conExp(*ptr,exponent))
+                {
+                    make(coefficient, exponent, ptr);
+                }
+
+                else
+                {
+                    struct node* temp2 = *ptr;
+                    while(temp2->exp != exponent)
+                        temp2 = temp2->next;
+
+                    temp2->coeff += coefficient;
+
+                }
                 ptr2 = ptr2->next;
             }
 
@@ -72,7 +102,10 @@ int main(void)
     make(2,0,&poly2);
     print(poly2);
 
+    make(0,-1,&poly);
+
     multiply(poly1,poly2,&poly);
 
+    poly = poly->next;
     print(poly);
 }
